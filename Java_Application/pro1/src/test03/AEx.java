9e3a2a96a2107c9f1eb7e96a89264ebe85e29521
Komorebi_Mac <karan1284@naver.com>
2023-09-02 12:10:41 +0900

    day1,third

---
+++ Java_Application/pro1/src/test03/AEx.java
@@ -0,0 +1,13 @@
+package test03;
+
+public class AEx {
+
+    public static void main(String[] args) {
+        //A 객체 생성
+        A a = new A();
+
+        //A 메소드 호출
+        a.method1(10);
+    }
+    
+}
