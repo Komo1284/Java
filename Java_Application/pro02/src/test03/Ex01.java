01a90c946e72acd8ea5655a97e57fad6ebbb1a2b
Komorebi_Mac <karan1284@naver.com>
2023-09-03 11:24:35 +0900

    day2. second

---
+++ Java_Application/pro02/src/test03/Ex01.java
@@ -0,0 +1,14 @@
+package test03;
+
+public class Ex01 {
+    public static void printLength(String data) {
+        int result = data.length();
+        System.out.println("문자 수: " + result);
+    }
+    public static void main(String[] args) {
+        System.out.println("[프로그램 시작]\n");
+        printLength("ThisIsJava");
+        printLength(null);
+        System.out.println("[프로그램 종료]");      
+    }
+}
