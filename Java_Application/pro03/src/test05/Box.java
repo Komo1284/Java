36443adea80db90883bd1f3e4f67dae6b6064b62
Komorebi_Mac <karan1284@naver.com>
2023-09-09 12:13:22 +0900

    day3.third

---
+++ Java_Application/pro03/src/test05/Box.java
@@ -0,0 +1,15 @@
+package test05;
+
+public class Box<T> {
+    //필드
+    private T t;
+    //Getter 메소드
+    public T get() {
+        return t;
+    }
+
+    //Setter 메소드
+    public void set(T t) {
+        this.t = t;
+    }
+}
