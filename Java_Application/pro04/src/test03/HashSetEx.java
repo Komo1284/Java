be84e50b7e1e71b9e26e04e172c44b22e9358bad
Komorebi_Mac <karan1284@naver.com>
2023-09-10 11:49:36 +0900

    day4.second

---
+++ Java_Application/pro04/src/test03/HashSetEx.java
@@ -0,0 +1,22 @@
+package test03;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        //HashSet 컬렉션 생성
+        Set<String> set = new HashSet<String>();
+        
+        //객체 저장
+        set.add("Java");
+        set.add("JDBD");
+        set.add("JSP");
+        set.add("Java"); //중복객체이므로 저장하지 않음
+        set.add("Spring");
+
+        //저장된 객체 수 출력
+        int size = set.size();
+        System.out.println("총 객체 수: " + size);
+    }
+}
