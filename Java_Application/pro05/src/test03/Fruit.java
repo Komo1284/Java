d4e4cee267b5055a1445720ef43c00417f62bfb7
Komorebi_Mac <karan1284@naver.com>
2023-09-16 11:07:03 +0900

    day5.second

---
+++ Java_Application/pro05/src/test03/Fruit.java
@@ -0,0 +1,11 @@
+package test03;
+
+public class Fruit {
+    public String name;
+    public int price;
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
