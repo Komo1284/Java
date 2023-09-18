38e867af3d1a69f83b4af5b6036364e0bdcae57c
Komorebi_Mac <karan1284@naver.com>
2023-09-18 14:52:53 +0900

    생활코딩

---
+++ 생활코딩/Java_Basic/src/usallycoding/LoginDemo.java
@@ -0,0 +1,12 @@
+package usallycoding;
+
+public class LoginDemo {
+    public static void main(String[] args) {
+        Stirng id = args[0];
+        if(id.equals("egoing")) {
+            System.out.println("right");
+        } else {
+            System.out.println("wrong");
+        }
+    }
+}
