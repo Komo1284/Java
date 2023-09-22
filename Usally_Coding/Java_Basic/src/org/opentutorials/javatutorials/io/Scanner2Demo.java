9f1d73811c17dcec55c61d7310e27586425bf734
Komorebi_Mac <karan1284@naver.com>
2023-09-22 22:48:33 +0900

    no message

--- Usally_Coding/Java_Basic/src/org/opentutorials/javatutorials/io/Scanner2Demo.java
+++ Usally_Coding/Java_Basic/src/org/opentutorials/javatutorials/io/Scanner2Demo.java
@@ -6,6 +6,7 @@ public class Scanner2Demo {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+		System.out.println(sc.next());
 		while (sc.hasNext()) {
 			System.out.println(sc.nextInt()*1000);
 		}
