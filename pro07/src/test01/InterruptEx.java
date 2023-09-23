113554994189d2be5da97c3e32345b8a9ab00dbc
Komorebi_Mac <karan1284@naver.com>
2023-09-23 09:20:08 +0900

    no message

---
+++ pro07/src/test01/InterruptEx.java
@@ -0,0 +1,17 @@
+package test01;
+
+public class InterruptEx {
+
+	public static void main(String[] args) {
+		Thread thread = new PrintThread();
+		thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {}
+		
+		thread.interrupt();
+
+	}
+
+}
---
+++ pro07/src/test01/PrintThread.java
@@ -0,0 +1,13 @@
+package test01;
+
+public class PrintThread {
+	public void run() {
+		try{
+			while (true) {
+				System.out.println("실행 중");
+				Thread.sleep(1);
+			}
+	} catch(InterruptedException e) {}
+	System.out.println("리소스 정리");
+	System.out.println("실행 종");
+}
