db883b084d664c8462c53006b14f3249da974919
Komorebi_Mac <karan1284@naver.com>
2023-09-23 09:28:20 +0900

    no message

--- pro07/src/test01/PrintThread.java
+++ pro07/src/test01/PrintThread.java
@@ -1,13 +1,14 @@
 package test01;
 
-public class PrintThread {
+public class PrintThread extends Thread {
 	public void run() {
 		try{
 			while (true) {
 				System.out.println("실행 중");
 				Thread.sleep(1);
 			}
-	} catch(InterruptedException e) {}
+		} catch(InterruptedException e) {}
 	System.out.println("리소스 정리");
 	System.out.println("실행 종");
+	}
 }
