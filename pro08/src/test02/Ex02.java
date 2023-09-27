798553bfcbe8ad10249036b0b1a3d18e7d5b9478
Komorebi_Mac <karan1284@naver.com>
2023-09-27 13:14:46 +0900

    day8.third

---
+++ pro08/src/test02/Ex02.java
@@ -0,0 +1,15 @@
+package test02;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		String data = new StringBuilder()
+				.append("DEF")
+				.insert(0, "ABC")
+				.delete(3, 4)
+				.toString();
+		System.out.println(data);
+
+	}
+
+}
